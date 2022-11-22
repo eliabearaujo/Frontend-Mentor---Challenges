d243c6ecec5cbce86cee89e04cc205d719e68536
eliabearaujo <eliabe_trajano@hotmail.com>
2022-11-22 01:40:58 -0300

    Adicionando Charts

--- Junior/expenses-chart-component-main/script.js
+++ Junior/expenses-chart-component-main/script.js
@@ -2,9 +2,10 @@ const chartsArea = document.querySelector('.charts');
 let date = new Date();
 let hoje = date.getDay();
 
+//Fetch JSON
 const data = fetch('./data.json')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    return data;
   })
   .catch((error) => console.log(error));
