d4f136210f053058a55e6b2f50edac734e11b5cd
eliabearaujo <eliabe_trajano@hotmail.com>
2022-11-25 03:33:07 -0300

    Cards de amount finalizados

--- Junior/expenses-chart-component-main/script.js
+++ Junior/expenses-chart-component-main/script.js
@@ -1,21 +1,35 @@
 const chartsArea = document.querySelector('.charts');
+
 let date = new Date();
 let hoje = date.getDay();
 
 //Fetch JSON
-const data = fetch('./data.json')
-  .then((response) => response.json())
-  .catch((error) => console.log(error));
+async function fecthData() {
+  const dataPromise = await fetch('./data.json');
+  const data = await dataPromise.json();
+  generateBars(data);
+}
 
-data.then((elementList) =>
+async function generateBars(elementList) {
   elementList.forEach((element) => {
     const chart = document.createElement('div');
     chart.classList.add('chart');
     chart.innerHTML = `
+    <div class="amount" style="display:none">$${element.amount}</div>
     <div class="bar" style="height:${element.amount * 1.5}%"></div>
     <div class="title">${element.day}</div>
-    <div class="amount">$${element.amount}</div>
     `;
     chartsArea.appendChild(chart);
-  })
-);
+  });
+  const bars = document.querySelectorAll('.bar');
+  const amounts = document.querySelectorAll('.amount');
+  bars.forEach((bar, index) => {
+    bar.addEventListener('mouseover', () => {
+      amounts[index].style.display = 'block';
+    });
+    bar.addEventListener('mouseleave', () => {
+      amounts[index].style.display = 'none';
+    });
+  });
+}
+fecthData();
